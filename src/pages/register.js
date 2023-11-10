0f29aecd066f2faeb2126a059eb549b27196c63c
ensarsnr <dev.anise@outlook.com>
2023-11-11 01:48:53 +0300

    Register process completed

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,17 +1,40 @@
+"use client";
+import services from "@/services/auth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 function Register() {
+  //hooks
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await services.register(username, email, pass);
+    console.log(response.data);
+
+    setUsername("");
+    setEmail("");
+    setPass("");
+
+    router.push("/chat");
+  };
+
   return (
     <div className="flex text-black col-span-4  flex-col justify-evenly   bg-white rounded-r-lg">
       <h1 className=" text-center font-bold text-transparent  p-10 text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-        asldk
+        Sign Up
       </h1>
-      <form className="w-3/4 m-auto">
+      <form onSubmit={handleSubmit} className="w-3/4 m-auto">
         <div className="mt-5 relative z-0 w-full group">
           <input
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-400 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
             required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
           <label
             htmlFor="floating_email"
@@ -26,8 +49,9 @@ function Register() {
             name="floating_email"
             id="floating_email"
             className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-400 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
             required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label
             htmlFor="floating_email"
@@ -42,8 +66,9 @@ function Register() {
             name="floating_email"
             id="floating_email"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-400 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
             required
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
           />
           <label
             htmlFor="floating_email"
@@ -53,11 +78,14 @@ function Register() {
           </label>
         </div>
         <div className="mt-10 text-center">
-          <button className="font-extrabold hover:duration-500 duration-500 py-2 px-3 rounded-lg bg-blue-500 hover:bg-blue-600 text-white w-2/3 ">
+          <button
+            type="submit"
+            className="font-extrabold hover:duration-500 duration-500 py-2 px-3 rounded-lg bg-blue-500 hover:bg-blue-600 text-white w-2/3 "
+          >
             Sign Up
           </button>
         </div>
-        <div className="text-center mb-5">
+        <div className="text-center mt-5">
           if you have an account
           <Link className="text-red-500" href="/login">
             {" "}
