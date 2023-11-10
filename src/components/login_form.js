0782faa9bc6bb1eec702f6954ee802dd7dcf9a3b
ensarsnr <dev.anise@outlook.com>
2023-11-11 01:46:31 +0300

    Login process completed

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -1,19 +1,42 @@
+import services from "@/services/auth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 function LoginForm() {
+  // hooks
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const router = useRouter();
+
+  // functions
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await services.login(email, pass);
+    if (response && response.data) {
+      console.log(response.data.message);
+      router.push("/chat");
+    } else {
+      console.error("Response or response.data is undefined.");
+    }
+    setEmail("");
+    setPass("");
+  };
+
   return (
     <div className="flex text-black col-span-4  flex-col justify-evenly   bg-white rounded-r-lg">
       <h1 className=" text-center font-bold text-transparent  p-10 text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         Sign In
       </h1>
-      <form className="w-3/4 m-auto">
+      <form onSubmit={handleSubmit} className="w-3/4 m-auto">
         <div className="mb-10 relative z-0 w-full group">
           <input
             type="email"
             name="floating_email"
             id="floating_email"
             className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-400 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
           <label
@@ -29,7 +52,8 @@ function LoginForm() {
             name="floating_email"
             id="floating_email"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-400 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
             required
           />
           <label
@@ -40,7 +64,10 @@ function LoginForm() {
           </label>
         </div>
         <div className="mt-10 text-center">
-          <button className="font-extrabold hover:duration-500 duration-500 py-2 px-3 rounded-lg bg-blue-500 hover:bg-blue-600 text-white w-2/3 ">
+          <button
+            type="submit"
+            className="font-extrabold hover:duration-500 duration-500 py-2 px-3 rounded-lg bg-blue-500 hover:bg-blue-600 text-white w-2/3 "
+          >
             Sign In
           </button>
         </div>
