81d74c0765639e079702af863c2d988169888d00
ensarsnr <dev.anise@outlook.com>
2023-11-12 05:20:06 +0300

    Updated page and get router data

--- src/components/persons.js
+++ src/components/persons.js
@@ -1,23 +1,30 @@
 import React from "react";
-import MiniProfile from "./mini_profile";
-
-function Persons() {
+// import MiniProfile from "./mini_profile";
+function Persons({ username, userId }) {
   return (
     <div className="col-span-2 bg-slate-400">
-      <MiniProfile />
-      <div className="mt-10 text-center bg-white w-11/12 rounded-2xl m-auto">
+      <div className="flex m-3 ">
+        <div className="bg-black rounded-full w-10 h-10 text-center">s</div>
+        <div className="ml-3 text-black">
+          <h1>{username}</h1>
+        </div>
+      </div>
+      <span className="ml-3 text-sm text-gray-600">
+        ID: <span style={{ fontSize: "12px" }}>{userId}</span>
+      </span>
+      <div className="mt-2 text-center bg-white w-11/12 rounded-2xl m-auto">
         <div className="p-5">
           <form>
             <label
-              for="default-search"
-              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
             >
               Search
             </label>
-            <div class="relative">
-              <div class="absolute inset-y-0 start-0 flex items-center ps-5 pointer-events-none">
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-5 pointer-events-none">
                 <svg
-                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -25,9 +32,9 @@ function Persons() {
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                   />
                 </svg>
@@ -35,7 +42,8 @@ function Persons() {
               <input
                 type="search"
                 id="default-search"
-                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-300 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                name="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-300 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search Mockups, Logos..."
                 required
               />
