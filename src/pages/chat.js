6886b2cda670cb830a5cafc0a3cafd93f6c27af8
ensarsnr <dev.anise@outlook.com>
2023-11-15 00:20:52 +0300

    Created function to send userid to message component

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../app/globals.css";
 import Persons from "../components/persons";
 import Message from "@/components/message";
@@ -8,12 +8,21 @@ function Chat() {
   // router'dan gelen yani login veya register sayfasından gelen username bilgisini bu şekilde alıyorum
   const router = useRouter();
   const { username, userId } = router.query;
-  console.log("userid:", userId);
+  const [selectedUser, setSelectedUser] = useState("");
+
+  const personClick = (userId) => {
+    setSelectedUser(userId);
+  };
+
   return (
     <div className="flex h-screen ">
       <div className="grid grid-cols-7 shadow-lg m-auto w-9/12 rounded-md bg-white h-5/6">
-        <Persons username={username} userId={userId} />
-        <Message />
+        <Persons
+          onPersonClick={personClick}
+          userId={userId}
+          username={username}
+        />
+        <Message selectedUser={selectedUser} />
       </div>
     </div>
   );
