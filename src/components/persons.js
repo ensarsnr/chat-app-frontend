9af5341f77409b3345357a64a69345620d7caf9b
ensarsnr <dev.anise@outlook.com>
2023-11-15 00:19:59 +0300

    User data was extracted from the db and edits were made on the page.

--- src/components/persons.js
+++ src/components/persons.js
@@ -1,26 +1,69 @@
-import React from "react";
-// import MiniProfile from "./mini_profile";
-function Persons({ username, userId }) {
+"use client";
+import userServices from "@/services/user";
+import React, { useEffect, useState } from "react";
+import { IoIosSettings } from "react-icons/io";
+import Avatar from "./avatar";
+
+function Persons({ onPersonClick, username, userId }) {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [search, setSearch] = useState("");
+  const [friends, setFriends] = useState([]);
+  const [pic, setPic] = useState("");
+
+  const getName = async (userId) => {
+    try {
+      const response = await userServices.getName(userId);
+      setSurname(response.data.surname);
+      setName(response.data.name);
+      setPic(response.data.pic);
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  };
+
+  const allFriends = async (userId) => {
+    try {
+      const response = await userServices.allFriends(userId);
+      setFriends(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const searchFriends = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    getName(userId);
+    allFriends(userId);
+  }, [userId]);
+
+  //
+  const filteredFriends = friends.filter((friend) =>
+    friend.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="col-span-2 bg-slate-400">
-      <div className="flex m-3 ">
-        <div className="bg-black rounded-full w-10 h-10 text-center">s</div>
-        <div className="ml-3 text-black">
-          <h1>{username}</h1>
+    <div className="col-span-2 bg-gray-100 rounded-l-lg">
+      <div className="justify-between flex m-3 ">
+        <div className="flex">
+          <Avatar pic={pic} />
+          <div className="ml-3 text-black">
+            <h1>
+              {name} {surname}
+            </h1>
+            <span className="text-sm text-gray-600">#{username}</span>
+          </div>
+        </div>
+        <div className="text-black mr-3 justify-center items-center flex">
+          <IoIosSettings size={20} color="gray" />
         </div>
       </div>
-      <span className="ml-3 text-sm text-gray-600">
-        ID: <span style={{ fontSize: "12px" }}>{userId}</span>
-      </span>
-      <div className="mt-2 text-center bg-white w-11/12 rounded-2xl m-auto">
+      <div className="mt-2 text-center bg-white w-11/12 shadow-lg rounded-2xl m-auto">
         <div className="p-5">
           <form>
-            <label
-              htmlFor="default-search"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Search
-            </label>
             <div className="relative">
               <div className="absolute inset-y-0 start-0 flex items-center ps-5 pointer-events-none">
                 <svg
@@ -43,9 +86,11 @@ function Persons({ username, userId }) {
                 type="search"
                 id="default-search"
                 name="default-search"
-                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-300 dark:border-gray-300 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos..."
+                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-100 dark:border-gray-300 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Friend"
                 required
+                value={search}
+                onChange={searchFriends}
               />
             </div>
           </form>
@@ -56,14 +101,24 @@ function Persons({ username, userId }) {
               style={{ maxHeight: "300px", overflowY: "auto" }}
             >
               {/* Container with fixed height and scrollable */}
-              {[1, 2, 3, 4, 5, 6, 7].map((index) => (
-                <div key={index} className="mt-5 justify-between flex">
-                  <div className="rounded-full w-10 h-10 bg-red-500">w</div>
-                  <div className="ml-3">
-                    <h1 className="text-sm">deneme</h1>
+              {filteredFriends.map((index) => (
+                <div
+                  onClick={() => onPersonClick(index._id)}
+                  key={index}
+                  className="justify-between mt-5 flex cursor-pointer hover:bg-gray-100 p-2 duration-300 hover:duration-300 rounded-lg "
+                >
+                  <Avatar pic={pic} />
+                  <div className="w-3/5 ml-3">
+                    <h1 className="text-sm">
+                      {index.name} {index.username}
+                    </h1>
                     <div className="text-xs text-gray-600">deneme</div>
                   </div>
-                  <div>asdasd</div>
+                  <div className="flex items-center">
+                    <div className="bg-red-600 text-white rounded-full w-6 h-6 mr-2 flex items-center justify-center">
+                      <span>1</span>
+                    </div>
+                  </div>
                 </div>
               ))}
             </div>
