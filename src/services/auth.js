6b8b934147b74cca4fb1e4d4f3f5813e6d57f83b
ensarsnr <dev.anise@outlook.com>
2023-11-15 00:21:36 +0300

    Added user name and surname

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,9 +4,11 @@ const api = axios.create({
   baseURL: "http://localhost:3001",
 });
 
-const register = async (username, email, pass) => {
+const register = async (name, surname, username, email, pass) => {
   try {
     const response = await api.post("/auth/add", {
+      name: name,
+      surname: surname,
       username: username,
       email: email,
       pass: pass,
