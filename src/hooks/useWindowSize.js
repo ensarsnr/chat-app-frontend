173baa72b8cd8eec3c56ecab8d283b196f934cb6
ensarsnr <dev.anise@outlook.com>
2023-11-19 21:54:02 +0300

    Created hook for  window size

---
+++ src/hooks/useWindowSize.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function useWindowSize() {
+  const isClient = typeof window === "object";
+
+  const [windowSize, setWindowSize] = useState({
+    width: isClient ? window.innerWidth : undefined,
+    height: isClient ? window.innerHeight : undefined,
+  });
+
+  useEffect(() => {
+    if (!isClient) {
+      return;
+    }
+
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [isClient]);
+
+  return windowSize;
+}
+
+export default useWindowSize;
