ba95151d4c5754f846f37cd8aefe7aa870d33887
ensarsnr <dev.anise@outlook.com>
2023-11-22 01:27:30 +0300

    Fixed bug

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,8 +1,22 @@
-import React from "react";
+import friendService from "@/services/friend";
+import React, { useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { MdDarkMode } from "react-icons/md";
 
-function Settings({ backChat }) {
+function Settings({ userId, backChat }) {
+  const [username, setUsername] = useState("");
+
+  const addToFriend = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await friendService.addFriend(userId, username);
+      console.log("responseFriend: ", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="rounded-lg bg-gray-100 grid grid-rows-5 col-span-7">
       {/* HEADERS */}
@@ -10,6 +24,7 @@ function Settings({ backChat }) {
         <div class="p-4 rounded-t">
           <IoIosArrowBack
             color="gray"
+            addToFriend
             type="button"
             onClick={backChat}
             size={40}
@@ -27,15 +42,29 @@ function Settings({ backChat }) {
         </div>
       </div>
       <div className="border-t-2 border-gray-400 bg-gray-100 col-span-7 row-span-4 flex justify-between ">
-        <div className="w-1/6 border-r-2 border-gray-400">
-          <h1 className="text-center text-black">Friend Requests</h1>
-          {/* Buraya arkadaş istekleri gelecek */}
-          <div className="bg-red-300 text-black mt-5">n-y</div>
-          <h1 className="text-black">Added Friend</h1>
-          <form></form>
+        <div className="col-span-4  m-auto">
+          <h1 className="text-center text-black mb-5">Added Friend</h1>
+          <form onSubmit={addToFriend} className="mt-5 text-center">
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="block w-full p-2  text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-100 dark:border-gray-300 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Frind Username"
+              required
+            />
+            <button
+              type="submit"
+              className="w-full mt-5 py-2 px-3 bg-blue-500 duration-100 hover:duration-100 hover:bg-blue-600 rounded-lg"
+            >
+              Added
+            </button>
+          </form>
         </div>
-        <div className=" w-4/6">asd</div>
-        <div className="bg-gray-200 w-1/6">asd</div>
+        <div className="col-span-3 m-auto text-black">
+          <div>
+            <h1>Friend Requests</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
