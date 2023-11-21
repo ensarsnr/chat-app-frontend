ad0ab56edb48a724d5071a645a223ddf92c5a902
ensarsnr <dev.anise@outlook.com>
2023-11-22 01:27:53 +0300

    Design done

--- src/components/start.js
+++ src/components/start.js
@@ -1,7 +1,10 @@
 import useWindowSize from "@/hooks/useWindowSize";
 import React from "react";
+import Image from "next/image";
+import startpng from "../assets/images/start.png";
+import Settings from "./settings";
 
-function Start() {
+function Start({ openSettings }) {
   const { width, height } = useWindowSize();
 
   const maxContainerHeight = height >= 959 ? 670 : 500;
@@ -11,8 +14,26 @@ function Start() {
   };
 
   return (
-    <div style={containerStyle} className="flex justify-center items-center">
-      <div>start</div>
+    <div
+      style={containerStyle}
+      className="h-5/6 items-center rounded-l-lg flex flex-col justify-around col-span-3"
+    >
+      <div>
+        <h2 className="font-bold text-2xl text-blue-500">Welcome to the App</h2>
+      </div>
+      <div>
+        <div>
+          <Image src={startpng} className="ml-20" width={200} height={200} />
+        </div>
+      </div>
+      <div>
+        <button
+          onClick={openSettings}
+          className="bg-green-500 px-4 py-2 rounded-lg text-white"
+        >
+          Add a friend
+        </button>
+      </div>
     </div>
   );
 }
