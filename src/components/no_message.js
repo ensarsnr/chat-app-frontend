c7df7ec6e0e112926c4c96d951e2bb7841c71db2
ensarsnr <dev.anise@outlook.com>
2023-11-21 22:59:47 +0300

    Created component

---
+++ src/components/no_message.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoMessage() {
+  return (
+    <div className="flex justify-center h-screen items-center">
+      <div className="m-auto">No massge</div>
+    </div>
+  );
+}
+
+export default NoMessage;
